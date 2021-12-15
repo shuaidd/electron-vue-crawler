ee393da9f31026c42348743519f665e1fc9b7b1b
ddshuai <ddshuai@shinyway.com.cn>
2021-12-15 15:53:46 +0800

    初始化

---
+++ src/renderer/pages/TestScala.vue
@@ -0,0 +1,17 @@
+
+<template>
+   <div>
+       <h1>{{msg}}</h1>
+   </div>
+</template>
+
+<script>
+export default {
+    name: "TestScala",
+    data() {
+        return {
+            msg: 'hello'
+        }
+    }
+}
+</script>
